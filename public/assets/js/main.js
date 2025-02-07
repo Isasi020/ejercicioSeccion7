85ff61ce8f74ae404efd6f370a05bcf7a58e710d
Juan Francisco <jfsanchez@ext.chequemotiva.com.>
2025-02-07 11:31:43 +0100

    añadido listado pelis

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,35 +1,33 @@
-
-
-document.addEventListener('DOMContentLoaded', function(){
-
-    $('form#buscarPeli').on('submit', async function(event) {
+document.addEventListener('DOMContentLoaded', function () {
+    $('form#buscarPeli').on('submit', async function (event) {
         event.preventDefault();
 
         const params = $(this).serialize();
-        const url =`${$(this).attr('action')}?${params}`;
-        
+        const url = `${$(this).attr('action')}?${params}`;
+
         const call = await fetch(url);
         const response = await call.json();
 
-        let message = "";
-        for(let movie of response){
-            message += `Titulo: ${movie.Title}, id: ${movie.imdbID}\n`;
-        }
-        alert(message);
+        const $moviesList = $('ul#moviesList');
+
+        $moviesList.empty();
+
+        response.forEach(movie => {
+            $moviesList.append(`<li>Titulo: ${movie.Title}, id: ${movie.imdbID}</li>`)
+        });
     });
 
-    
-    $('form#buscarPeliPorId').on('submit', async function(event) {
+
+    $('form#buscarPeliPorId').on('submit', async function (event) {
         event.preventDefault();
 
         const params = $(this).serialize();
-        const url =`${$(this).attr('action')}?${params}`;
-        
+        const url = `${$(this).attr('action')}?${params}`;
+
         const call = await fetch(url);
         const response = await call.json();
 
-        alert(JSON.stringify(response, null, 2)); 
-    
-    });
+        alert(JSON.stringify(response, null, 2));
 
+    });
 });
